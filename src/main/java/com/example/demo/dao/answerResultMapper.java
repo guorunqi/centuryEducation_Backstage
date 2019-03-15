b1f1d2ff98278fe19251f7afc63450ff44cf5f45
weishouhao <weishouhaoatt@163.com>
2019-03-15 09:20:17 +0800

    删除没用的文件

--- src/main/java/com/example/demo/dao/answerResultMapper.java
+++ src/main/java/com/example/demo/dao/answerResultMapper.java
@@ -1,30 +1,30 @@
 package com.example.demo.dao;
 
-import com.example.demo.domain.answerResult;
-import com.example.demo.domain.answerResultExample;
+import com.example.demo.domain.AnswerResult;
+import com.example.demo.domain.AnswerResultExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
-public interface answerResultMapper {
-    int countByExample(answerResultExample example);
+public interface AnswerResultMapper {
+    int countByExample(AnswerResultExample example);
 
-    int deleteByExample(answerResultExample example);
+    int deleteByExample(AnswerResultExample example);
 
     int deleteByPrimaryKey(String id);
 
-    int insert(answerResult record);
+    int insert(AnswerResult record);
 
-    int insertSelective(answerResult record);
+    int insertSelective(AnswerResult record);
 
-    List<answerResult> selectByExample(answerResultExample example);
+    List<AnswerResult> selectByExample(AnswerResultExample example);
 
-    answerResult selectByPrimaryKey(String id);
+    AnswerResult selectByPrimaryKey(String id);
 
-    int updateByExampleSelective(@Param("record") answerResult record, @Param("example") answerResultExample example);
+    int updateByExampleSelective(@Param("record") AnswerResult record, @Param("example") AnswerResultExample example);
 
-    int updateByExample(@Param("record") answerResult record, @Param("example") answerResultExample example);
+    int updateByExample(@Param("record") AnswerResult record, @Param("example") AnswerResultExample example);
 
-    int updateByPrimaryKeySelective(answerResult record);
+    int updateByPrimaryKeySelective(AnswerResult record);
 
-    int updateByPrimaryKey(answerResult record);
+    int updateByPrimaryKey(AnswerResult record);
 }
